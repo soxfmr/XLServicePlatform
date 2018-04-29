9b054801c97a0eef226406d585cb63a3f069f84c
Soxfmr <soxfmr@foxmail.com>
2018-04-29 22:21:06 +0800

    XLServicePlatform DLL export

--- XLServicePlatform/main.c
+++ XLServicePlatform/main.c
@@ -1,7 +1,51 @@
 #include <Windows.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPWSTR lpCmdLine, int nCmdShow)
+__declspec(dllexport) BOOL IsInstalled()
+{
+	OutputDebugString(L"IsInstalled");
+	return TRUE;
+}
+
+__declspec(dllexport) BOOL InstallPlatform()
+{
+	OutputDebugString(L"InstallPlatform");
+	return TRUE;
+}
+
+__declspec(dllexport) BOOL UnInstallerPlatform()
+{
+	OutputDebugString(L"UnInstallerPlatform");
+	return TRUE;
+}
+
+__declspec(dllexport) BOOL StartPlatform()
+{
+	OutputDebugString(L"StartPlatform");
+	return TRUE;
+}
+
+__declspec(dllexport) BOOL StopServicePlatform()
+{
+	OutputDebugString(L"StopServicePlatform");
+	return TRUE;
+}
+
+__declspec(dllexport) BOOL SetPlatformAutoRun()
+{
+	OutputDebugString(L"SetPlatformAutoRun");
+	return TRUE;
+}
+
+__declspec(dllexport) VOID WINAPI ServiceMain(DWORD dwArgc, LPTSTR *lpszArgv)
+{
+	return;
+}
+
+BOOL WINAPI DllMain(
+	_In_ HINSTANCE hinstDLL,
+	_In_ DWORD     fdwReason,
+	_In_ LPVOID    lpvReserved
+)
 {
 	return 0;
 }
